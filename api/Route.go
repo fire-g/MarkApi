eaba788d77f64935e4eeecfc6207abb7ebd33895
HaoTian <18296517259@163.com>
2021-11-24 22:38:46 +0800

    first commit

---
+++ api/Route.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"net/http"
+
+	"mark-api/route"
+)
+
+var (
+	Router = route.New()
+)
+
+func Get(pattern string, handler http.HandlerFunc) {
+	Router.Get(pattern, handler)
+}
+
+func Post(pattern string, handler http.HandlerFunc) {
+	Router.Post(pattern, handler)
+}
+
+func Put(pattern string, handler http.HandlerFunc) {
+	Router.Del(pattern, handler)
+}
+
+func Del(pattern string, handler http.HandlerFunc) {
+	Router.Del(pattern, handler)
+}
